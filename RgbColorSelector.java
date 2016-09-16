ae6a328efd967a5ec19ea34683b2f9010007b610
nauke <kkavalchuk@gmail.com>
2016-09-16 12:32:31 +0200

    Add javadoc; save in .csv only numerical values; code refactoring

--- RgbColorSelector.java
+++ RgbColorSelector.java
@@ -1,39 +1,44 @@
+/**
+ * RGB Color Selector
+ * 1. select the RGB values by using sliders or by entering numeric values 
+ * and view the resulting color
+ * 2.save RGB values in .ser or .csv format
+ */
+
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.swing.JMenuItem;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JSlider;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
+import java.awt.Font;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import javax.swing.BoxLayout;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-
-import java.io.Serializable;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
-import java.io.IOException;
-import java.io.File;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.BufferedReader;
-import java.io.FileReader;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
 
 public class RgbColorSelector implements Serializable {
 
-	Rgb rgb; // color that holds r, g and b values
+	Rgb rgb; // object that holds r, g and b values for color
 	private JTextField fieldR;
 	private JTextField fieldG;
 	private JTextField fieldB;
@@ -53,11 +58,11 @@ public class RgbColorSelector implements Serializable {
 		rgb = new Rgb();
 		frame = new JFrame("RGB Color Selector");
 		
-		// 0. contentPanel - main panel, containes other panels
+		// 1. contentPanel - main panel, containes other panels
 		JPanel contentPanel = new JPanel();
 		contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
 
-		// 1. menu bar
+		// 2. menu bar with save/open file options
 		JMenuBar menuBar = new JMenuBar();
 		JMenu menuSave = new JMenu("Save");
 		JMenuItem saveObjectMenuItem = new JMenuItem("Save as object (.ser)");
@@ -78,12 +83,12 @@ public class RgbColorSelector implements Serializable {
 		menuBar.add(menuSave);
 		menuBar.add(menuOpen);
 		
-		// 2. colorPanel - panel showing the color
+		// 3. colorPanel - panel showing the color
 		colorPanel = new JPanel();
 		colorPanel.setBackground(new Color(0, 0, 0));
 		colorPanel.setPreferredSize(new Dimension(100, 50));
 		
-		// 3. three panels (R, G, B) with sliders and corresponding text fields
+		// 4. three panels (R, G, B) with sliders and corresponding text fields
 		JPanel panelR = new JPanel();
 		JPanel panelG = new JPanel();
 		JPanel panelB = new JPanel();
@@ -121,7 +126,7 @@ public class RgbColorSelector implements Serializable {
 		panelB.add(sliderB);
 		panelB.add(fieldB);
 		
-		// 4. panel to display error messages
+		// 5. panel to display error messages
 		errorOutput = new JLabel(" ");
 		errorOutput.setFont(new Font("Serif", Font.PLAIN, 11));
 		errorOutput.setForeground(Color.RED);
@@ -138,13 +143,6 @@ public class RgbColorSelector implements Serializable {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
-	}
-	
-	private void resetAfterError() {
-		errorOutput.setText(" ");
-		fieldR.setBackground(Color.WHITE);
-		fieldG.setBackground(Color.WHITE);
-		fieldB.setBackground(Color.WHITE);
 	}
 	
 	class SliderListener implements ChangeListener {
@@ -256,7 +254,6 @@ public class RgbColorSelector implements Serializable {
 				File file = fc.getSelectedFile();
 				try {
 					BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-					writer.write("R,G,B\n");
 					writer.write(rgb.getRed() + "," + rgb.getGreen() + "," + rgb.getBlue());
 					writer.close();
 				} catch(IOException ex) {
@@ -278,16 +275,12 @@ public class RgbColorSelector implements Serializable {
 				try {
 					BufferedReader reader = new BufferedReader(new FileReader(file));
 					String line = null;
-					int lineCounter = 0;
 					while((line = reader.readLine()) != null) { 
-						if(lineCounter==1) { //skip first line "R,G,B"
-							String[] tokens = line.split(",");
-							rgb.setRed(Integer.parseInt(tokens[0]));
-							rgb.setGreen(Integer.parseInt(tokens[1]));
-							rgb.setBlue(Integer.parseInt(tokens[2]));
-							setNewValues();
-						}
-						lineCounter++;
+						String[] tokens = line.split(",");
+						rgb.setRed(Integer.parseInt(tokens[0]));
+						rgb.setGreen(Integer.parseInt(tokens[1]));
+						rgb.setBlue(Integer.parseInt(tokens[2]));
+						setNewValues();
 					}
 					reader.close();
 				} catch(IOException ex) {
@@ -306,4 +299,12 @@ public class RgbColorSelector implements Serializable {
 		sliderB.setValue(rgb.getBlue());
 		colorPanel.setBackground(new Color(rgb.getRed(), rgb.getGreen(), rgb.getBlue()));
 	}
+	
+	private void resetAfterError() {
+		errorOutput.setText(" ");
+		fieldR.setBackground(Color.WHITE);
+		fieldG.setBackground(Color.WHITE);
+		fieldB.setBackground(Color.WHITE);
+	}
 }
+
